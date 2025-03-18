6fe067c80f2a01674f085aa80a4f53fa9cc24bfa
Shahrear Ahamed <its.shahrear@gmail.com>
2025-03-18 10:10:45 +0600

    Change css for single details pages.

--- src/pages/work/WorkDetail.jsx
+++ src/pages/work/WorkDetail.jsx
@@ -3,25 +3,23 @@ import { useParams } from "react-router";
 import workData from "./work.json";
 
 const WorkDetail = () => {
-   const { id } = useParams();
-   const record = workData.find((item) => item.id === parseInt(id));
+  const { id } = useParams();
+  const record = workData.find((item) => item.id === parseInt(id));
 
-   if (!record) {
-      return <div>Record not found</div>;
-   }
+  if (!record) {
+    return <div>Record not found</div>;
+  }
 
-   return (
-      <main>
-         <h3>{record.title}</h3>
-         <div className="item-detail">
-            <span className="material-icons-sharp item-icon">
-               {record.icon}
-            </span>
-            <h4>{record.title}</h4>
-            <p>{record.description}</p>
-         </div>
-      </main>
-   );
+  return (
+    <main className="content">
+      <h3>{record.title}</h3>
+      <div className="item-detail">
+        <span className="material-icons-sharp item-icon">{record.icon}</span>
+        <h4>{record.title}</h4>
+        <p>{record.description}</p>
+      </div>
+    </main>
+  );
 };
 
 export default WorkDetail;
