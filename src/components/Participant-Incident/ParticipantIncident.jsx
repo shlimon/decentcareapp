e4e6e3bedef714dec25578befbc6f9fca0b7b420
MuhammadAmitHasan <bhamithasan@gmail.com>
2025-10-19 09:14:49 +0600

    Data layout created

---
+++ src/components/Participant-Incident/ParticipantIncident.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ParticipantIncident = () => {
+  return <div>Participant Incident</div>;
+};
+
+export default React.memo(ParticipantIncident);
---
+++ src/pages/MedicationPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Medication from '../components/Medication/Medication';
+
+const MedicationPage = () => {
+  return <Medication />;
+};
+
+export default React.memo(MedicationPage);
