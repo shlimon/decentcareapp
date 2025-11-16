ad43429ce884f3512bd0516246eacca7cec35a3e
nayeemgub <nayeem.eduplay@gmail.com>
2025-11-16 07:49:22 +0600

    id show

--- src/components/document-data/ShowDocument.jsx
+++ src/components/document-data/ShowDocument.jsx
@@ -4,49 +4,41 @@ function OptionsSelection({ setCurrentView }) {
    const options = [
       {
          id: 'document-1',
-         //  icon: '',
          title: 'Document 1',
          description: 'This is document 1',
       },
       {
          id: 'document-2',
-         //  icon: '',
          title: 'Document 2',
          description: 'This is document 2',
       },
       {
          id: 'document-3',
-         //  icon: '',
          title: 'Document 3',
          description: 'This is document 3',
       },
       {
          id: 'document-4',
-         //  icon: '',
-         title: 'Document 4 ',
+         title: 'Document 4',
          description: 'This is document 4',
       },
       {
          id: 'document-5',
-         //  icon: '',
          title: 'Document 5',
          description: 'This is document 5',
       },
       {
          id: 'document-6',
-         //  icon: '',
          title: 'Document 6',
          description: 'This is document 6',
       },
       {
          id: 'document-7',
-         //  icon: '',
          title: 'Document 7',
          description: 'This is document 7',
       },
       {
          id: 'document-8',
-         //  icon: '',
          title: 'Document 8',
          description: 'This is document 8',
       },
@@ -57,10 +49,8 @@ function OptionsSelection({ setCurrentView }) {
          <div className="options-grid">
             {options.map((option) => (
                <div key={option.id} className="option-card">
-                  {/* <div className="icon">{option.icon}</div> */}
                   <h3 className="text-nowrap">{option.title}</h3>
                   <p>{option.description}</p>
-
                   <button
                      className="btn"
                      style={{ width: 'auto', padding: '8px 16px' }}
@@ -75,8 +65,11 @@ function OptionsSelection({ setCurrentView }) {
 }
 
 const ShowDocument = () => {
-   const [currentView, setCurrentView] = useState('options');
+   const user = localStorage.getItem('user_data');
+   const userData = JSON.parse(user);
+   console.log('Staff ID:', userData.user._id);
 
+   const [currentView, setCurrentView] = useState('options');
    const handleBack = useCallback(() => setCurrentView('options'), []);
 
    return (
@@ -91,7 +84,6 @@ const ShowDocument = () => {
                   ← Back to Options
                </button>
             )}
-
             {currentView === 'options' && (
                <OptionsSelection setCurrentView={setCurrentView} />
             )}
