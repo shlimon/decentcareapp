de0bf56580bee7c7b8d2d0b7540dde96b6acbbf6
MuhammadAmitHasan <bhamithasan@gmail.com>
2025-11-23 19:19:31 +0600

    App UI update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,18 @@
-import React from "react";
-import { NavLink } from "react-router";
-
+import React from 'react';
+import { NavLink } from 'react-router';
 
 const navBars = [
-  { id: 1, icon: "dashboard", to: "/" },
-  { id: 2, icon: "assignment", to: "/forms" },
-  { id: 3, icon: "business_center", to: "/work" },
-  { id: 4, icon: "campaign", to: "/announce" },
-  { id: 5, icon: "hub", to: "/resource" },
-  { id: 6, icon: "account_circle", to: "/profile" }
+  { id: 1, icon: 'dashboard', to: '/' },
+  { id: 2, icon: 'assignment', to: '/forms' },
+  { id: 3, icon: 'business_center', to: '/work' },
+  { id: 4, icon: 'campaign', to: '/announce' },
+  { id: 5, icon: 'hub', to: '/resource' },
+  { id: 6, icon: 'account_circle', to: '/profile' },
 ];
 
 export default function Navbar() {
   return (
-    <nav className="border border-gray-500 max-w-[400px] fixed bottom-2.5 left-1/2 -translate-x-1/2 !rounded-full bg-white">
+    <nav className="border border-gray-500 max-w-[400px] fixed bottom-5 left-1/2 -translate-x-1/2 !rounded-full bg-white">
       <div className="nav-btn-box">
         {navBars.map((nav) => (
           <div key={nav.id} className="nav-btn">
@@ -24,7 +23,7 @@ export default function Navbar() {
               {({ isActive }) => (
                 <span
                   className="material-icons-sharp btn"
-                  style={{ color: isActive ? "purple" : "#6a7282" }}
+                  style={{ color: isActive ? 'purple' : '#6a7282' }}
                 >
                   {nav.icon}
                 </span>
---
+++ src/components/reusable/breadCrumb/BreadCrumb.jsx
@@ -0,0 +1,31 @@
+const BreadCrumb = ({ currentPage, prevPage, navigation, icon }) => {
+  return (
+    <nav aria-label="Breadcrumb" className="flex">
+      <ol className="flex overflow-hidden text-gray-600 border border-gray-200 rounded-lg">
+        <li className="flex items-center">
+          <div
+            role="button"
+            onClick={navigation}
+            className="flex h-10 items-center gap-1.5 bg-gray-100 px-4 transition hover:text-gray-900"
+          >
+            {icon}
+            <span className="ms-1.5 text-xs font-medium capitalize">
+              {' '}
+              {prevPage}{' '}
+            </span>
+          </div>
+        </li>
+
+        <li className="relative flex items-center">
+          <span className="absolute inset-y-0 -start-px h-10 w-4 bg-gray-100 [clip-path:_polygon(0_0,_0%_100%,_100%_50%)] rtl:rotate-180"></span>
+
+          <div className="flex items-center h-10 text-xs font-medium capitalize transition bg-white pe-4 ps-8 hover:text-gray-900">
+            {currentPage}
+          </div>
+        </li>
+      </ol>
+    </nav>
+  );
+};
+
+export default BreadCrumb;
