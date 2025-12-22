97f006fb090405d20431ae250d2fa17382033025
nayeemgub <nayeem.eduplay@gmail.com>
2025-12-22 12:33:30 +0600

    conflits of interest

--- src/pages/conflict-of-interest/ConflictOfInterestPage.jsx
+++ src/pages/conflict-of-interest/ConflictOfInterestPage.jsx
@@ -1,7 +1,12 @@
 import ConflictOfInterestForm from '@components/Conflict-Of-Interest-comp/ConflictOfInterestForm';
+import useGetAllMyConflicts from '@hooks/useGetAllMyConflicts';
 import React from 'react';
 
 const ConflictOfInterestPage = () => {
+   const { data: conflitsData, isLoading: isLoadingConflicts } =
+      useGetAllMyConflicts();
+
+   console.log('Conflicts of Interest Data:', conflitsData);
    return (
       <div>
          <ConflictOfInterestForm />
